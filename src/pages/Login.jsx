91be8c5130a45f70786656b50d6c802e76661a1f
Tanvi Bhalodiya <bhalodiyatanvi125@gmail.com>
2026-01-31 13:00:25 +0530

    [BT][pages added]

---
+++ src/pages/Login.jsx
@@ -0,0 +1,39 @@
+import { Link } from "react-router-dom";
+import  './Login.css';
+import React from 'react'
+
+const Login = () => {
+  return (
+   <div className="form-container">
+    <h1 className="form-title">Welcome Back</h1>
+    <form>
+      <div className="form-group">
+        <label htmlFor="email">Email Address</label>
+        <input 
+        type="email"
+        id="email"
+        name="email"
+        placeholder="Enter your email"
+        />
+      </div>
+      <div className="form-group">
+         <label htmlFor="password">password </label>
+        <input 
+        type="password"
+        id="password"
+        name="password"
+        placeholder="Enter your password"
+        />
+
+      </div>
+      <button type="submit " className="btn-primary">
+        Login
+      </button>
+    </form>
+   </div>
+  )
+}
+
+export default Login
+
+
