6be3a61690deb6086d7f087b9a5cb6fd346c6e4e
Tanvi Bhalodiya <bhalodiyatanvi125@gmail.com>
2026-02-03 21:29:01 +0530

    [BT][added]

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,42 +1,110 @@
-import { Link } from "react-router-dom";
-import  './Login.css';
-import React from 'react'
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import "./Login.css";
 
 const Login = () => {
+  
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const [errors, setErrors] = useState({});
+  const navigate = useNavigate();
+  
+  const validate = () => {
+    const newErrors = {};
+
+    if (!formData.email.trim()) {
+      newErrors.email = "Email is required";
+    } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(formData.email)) {
+      newErrors.email = "Invalid email format";
+    }
+
+    if (!formData.password.trim()) {
+      newErrors.password = "Password is required";
+    } else if (formData.password.length < 6) {
+      newErrors.password = "Minimum 6 characters required";
+    }
+
+    setErrors(newErrors);
+    return Object.keys(newErrors).length === 0;
+  };
+
+  const handleInputChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+
+    setErrors({
+      ...errors,
+      [e.target.name]: "",
+    });
+  };
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (validate()) {
+      const data = JSON.parse(localStorage.getItem("authData"));
+
+      if (
+        data &&
+        data.email === formData.email &&
+        data.password === formData.password
+      ) {
+        alert("Login successful ");
+        localStorage.setItem("loginData",JSON.stringify(formData))
+        navigate("/dashboard");
+      } else {
+        alert("Invalid email or password ");
+      }
+    }
+  };
+
   return (
-   <div className="form-container">
-    <h1 className="form-title">Welcome Back</h1>
-    <form>
-      <div className="form-group">
-        <label htmlFor="email">Email Address</label>
-        <input 
-        type="email"
-        id="email"
-        name="email"
-        placeholder="Enter your email"
-        />
-      </div>
-      <div className="form-group">
-         <label htmlFor="password">password </label>
-        <input 
-        type="password"
-        id="password"
-        name="password"
-        placeholder="Enter your password"
-        />
+    <div className="form-container">
+      <h1 className="form-title">LOGIN</h1>
 
-      </div>
-      <button type="submit " className="btn-primary">
-        Login
-      </button>
-    </form>
-    <p className="link-text">
-      Don't have an accout? <Link to="/register">Register here</Link>
-    </p>
-   </div>
-  )
-}
+      <form onSubmit={handleSubmit}>
+   
+        <div className="form-group">
+          <label>Email</label>
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            placeholder="Enter your email"
+            onChange={handleInputChange}
+          />
+          {errors.email && <span className="error-msg">{errors.email}</span>}
+        </div>
 
-export default Login
+     
+        <div className="form-group">
+          <label>Password</label>
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            placeholder="Enter your password"
+            onChange={handleInputChange}
+          />
+          {errors.password && (
+            <span className="error-msg">{errors.password}</span>
+          )}
+        </div>
 
+        <button type="submit" className="btn-primary">
+          Login
+        </button>
+      </form>
 
+      <p className="link-text">
+        Don't have an account? <Link to="/register">Register here</Link>
+      </p>
+    </div>
+  );
+};
+
+export default Login;
